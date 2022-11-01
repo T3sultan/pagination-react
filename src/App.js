ca463b6bb80b1d044f81d174591d921b5921076d
“T3sultan” <incognito.soul011@gmail.com>
2022-11-01 20:20:28 +0600

    done pagintation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Post from "./Post/Post";
+import Pagination from "./Pagination/Pagination";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(posts => setPosts(posts))
+      .catch(error => setError(error.message));
+  }, []);
+
+  if (error) return <h2>{error}</h2>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="  ">
+      {posts.length > 0 ? (
+        <>
+          <Pagination
+            className="container"
+            data={posts}
+            RenderComponent={Post}
+            title="Posts"
+            pageLimit={5}
+            dataLimit={9}
+          />
+        </>
+      ) : (
+        <h1>No Posts to display</h1>
+      )}
     </div>
   );
 }
